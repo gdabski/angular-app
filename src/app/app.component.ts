9ba782fffd0ca0577ea0da89c7bb8ffdc4b5afaa
gdabski <gdabski@gazeta.pl>
2016-10-18 23:00:17 +0200

    Refactorings at home.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,15 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  //templateUrl: './app.component.html'
-  template: `
-    <div class="container">
-      <h3>Angular Warsztat</h3>
-    <div class="columns">
-    <div class="column">
-    <todo-app></todo-app>
-  `,
-  styles: ['h1{ color: red}']
+@Component( {
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styles: ['h1{ color: red}']
 })
+
 export class AppComponent {
-  title = 'Angular seems to be working.. even!';
+    title = 'Angular seems to be working.. even!';
 
-  // <p> Count is: {{counter}} </p>
-  counter:number = 0;
+    counter: number = 0;
 
-  constructor(){
-
-    // setInterval(()=>{
-    //   this.counter = 5;
-    // },1000)
-
-  }
+    constructor() {}
 }
--- src/app/todo-app/todo-app.component.ts
+++ src/app/todo-app/todo-app.component.ts
@@ -2,58 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import { TodoListComponent } from './todo-list.component'
 import { Todo } from './interfaces';
 
-@Component({
-  selector: 'todo-app',
-  template: `
-  <button (click)="shown = todos">Todos</button>
-  <button (click)="shown = archived">Archived</button>
-  <div *ngIf="shown === todos">
-    <todo-list [data]="todos">
-      <div class="listheader"><b>Todo items:</b></div>
-      <div class="listfooter">Zakończonych zadań: {{completedTodos}}.</div>
-    </todo-list>
-    <button *unless="completedTodos" (click)=archiveCompleted()>Archiwizuj</button>
-    <todo-form (created)="add($event) "></todo-form>
-  </div>
-  <todo-list [data]="archived" *ngIf="shown === archived">
-    <div class="listheader"><b>Archived items:</b></div>
-    <div class="listfooter">All finished here.</div>
-  </todo-list>
-  `,
-  //encapsulation: ViewEncapsulation.Native,
-  styles: []
+@Component( {
+    selector: 'todo-app',
+    templateUrl: './todo-app.component.html',
+    //encapsulation: ViewEncapsulation.Native,
+    styles: []
 })
-export class TodoAppComponent implements OnInit {
+export class TodoAppComponent {
 
-  todos:Todo[] = [
-    {name: 'Todo A', completed: false},
-    {name: 'Next Todo', completed: true},
-    {name: 'Last Todo', completed: false},
-  ]
-  archived:Todo[] = []
-  shown:Todo[] = this.todos
+    todos: Todo[] = [
+        { name: 'Todo A', completed: false },
+        { name: 'Next Todo', completed: true },
+        { name: 'Last Todo', completed: false },
+    ]
+    archived: Todo[] = []
+    shown: Todo[] = this.todos
 
-  constructor() { }
-  
-  add(todo:Todo) {
-    this.todos.push(todo)
-  }
+    constructor() { }
 
-  archiveCompleted() {
-    let groups = this.todos.reduce((groups: {[key:string]: Todo[]}, todo: Todo) => {
-      todo.completed ? groups['archived'].push(todo) : groups['todos'].push(todo)
-      return groups;
-    }, {archived: [], todos: []} )
-    this.todos = groups['todos']
-    this.archived = this.archived.concat(groups['archived'])
-    this.shown = this.todos
-  }
+    add( todo: Todo ) {
+        this.todos.push( todo )
+    }
 
-  get completedTodos():number {
-    return this.todos.filter(todo => todo.completed).length
-  }
-  
-  ngOnInit() {
-  }
+
+    archiveCompleted() {
+        let groups = this.todos.reduce(( groups: { [key: string]: Todo[] }, todo: Todo ) => {
+            todo.completed ? groups['archived'].push( todo ) : groups['todos'].push( todo )
+            return groups;
+        }, { archived: [], todos: [] })
+        this.todos = groups['todos']
+        this.archived = this.archived.concat( groups['archived'] )
+        this.shown = this.todos
+    }
+
+    get completedTodos(): number {
+        return this.todos.filter( todo => todo.completed ).length
+    }
 
 }
